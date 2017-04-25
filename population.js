6551fcb092dd3f600a66b81eb5e121bb531711f7
i.jamlean <i.jamlean@mmguide.nl>
2017-04-25 16:58:14 +0200

    Added

---
+++ population.js
@@ -0,0 +1,22 @@
+var population = {
+    harvesters: function() {
+        return [
+            "harvester1",
+            "harvester2"
+            ];
+    },
+    upgraders: function() {
+        return [
+            "upgrader1",
+            "upgrader2"
+            ];
+    },
+    builders: function() {
+        return [
+            "builder1",
+            "builder2"
+            ];
+    }
+};
+
+module.exports = population;
